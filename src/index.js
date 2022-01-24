c1f79f99ea3b344eb83c1343236d3f78538f29a7
Unna27 <unnaweb27@gmail.com>
2022-01-24 11:04:26 +0100

    Integrated Atatus
    installed atatus-spa and added the import and config statement for atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('a4c700e98cc0404580d96c1ee5b3e2bd').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +24,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// atatus.notify(new Error('Test Atatus Setup')); // to test for attaus integration
