166bf560a8297a3870bab5a68216183a809be4cd
Unna27 <unnaweb27@gmail.com>
2022-01-27 11:44:22 +0100

    Added Warning alert for offline display

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
 import { getEvents, extractLocations } from './api';
+import { WarningAlert } from './Alert';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    eventCount: 32
+    eventCount: 32,
+    warnText: ''
   }
 
   // update events, when a suggestion list has been clicked in CitySearch
   updateEvents = (location, numEvents) => {
+    if(!navigator.onLine){
+      console.log(WarningAlert);
+    }
     getEvents().then((events) => {
       if(location!==undefined){
         const locationEvents = (location === 'all') ?
@@ -43,7 +48,8 @@ class App extends Component {
       if (this.mounted) {
         this.setState({ 
           events: events.slice(0, this.state.eventCount),
-          locations: extractLocations(events) 
+          locations: extractLocations(events),
+          warnText: !navigator.onLine ? 'You are offline. Events data displayed might not be accurate.' : ''
         });
       }
     });
@@ -59,6 +65,7 @@ class App extends Component {
           <h2>List of Training Events</h2>
           <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
           <NumberOfEvents eventCount={this.state.eventCount} updateEvents={this.updateEvents} />
+          <WarningAlert text={this.state.warnText} />
           <EventList events={this.state.events} />
         </div>
       );
