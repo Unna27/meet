b1b4c62d9e5f8b65313de7e062410e65b4e1135e
Unna27 <unnaweb27@gmail.com>
2022-01-28 15:38:39 +0100

    added scatterchart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NumberOfEvents from './NumberOfEvents';
 import WelcomeScreen from './WelcomeScreen';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import { WarningAlert } from './Alert';
+import { ScatterChart, Scatter, XAxis, YAxis, ZAxis, Legend, CartesianGrid, Tooltip } from 'recharts';
 
 class App extends Component {
   state = {
@@ -39,6 +40,17 @@ class App extends Component {
     });
    
   }
+
+// generates array of city and total number of events fro each city. Chart-data
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift() // to get the first value (city) from the array
+      return {city, number};
+    })
+    return data;
+  };
 
   async componentDidMount() {
     this.mounted = true;
@@ -72,13 +84,23 @@ class App extends Component {
           <h2>List of Training Events</h2>
           <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
           <NumberOfEvents eventCount={this.state.eventCount} updateEvents={this.updateEvents} />
+          <h4>Events in each City</h4>
           <WarningAlert text={this.state.warnText} />
+          <ScatterChart width={730} height={250}
+            margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            
+            <Scatter data={this.getData()} fill="#8884d8" />
+            <Scatter data={this.getData()} fill="#82ca9d" />
+          </ScatterChart>
           <EventList events={this.state.events} />
           <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
         </div>
       );
     }
   }
-  
-
+ 
 export default App;
